52e66314f5a383d64da4da8d80a9db38c430d845
Mathieu <albatros69@users.noreply.github.com>
2025-12-02 08:25:27 +0100

    Refacto input transformation

--- day-02/product_id.py
+++ day-02/product_id.py
@@ -6,16 +6,20 @@ lines = []
 for line in sys.stdin:
     lines.append(line.rstrip("\n"))
 
+intervals = [
+    tuple(int(a) for a in interval.split("-")) for interval in lines[0].split(",")
+]
+
 
 def is_invalid_p1(prod_id: str):
     l = len(prod_id) // 2
     return prod_id[:l] == prod_id[l:]
 
 
-result = 0
-for interval in lines[0].split(","):
-    start, end = (int(a) for a in interval.split("-"))
-    result += sum(a for a in range(start, end + 1) if is_invalid_p1(str(a)))
+result = sum(
+    sum(a for a in range(start, end + 1) if is_invalid_p1(str(a)))
+    for start, end in intervals
+)
 print("Part 1:", result)
 
 
@@ -24,9 +28,8 @@ def is_invalid_p2(prod_id: str):
     return any(prod_id == prod_id[:i] * (l // i) for i in range(1, l // 2 + 1))
 
 
-result = 0
-for interval in lines[0].split(","):
-    start, end = (int(a) for a in interval.split("-"))
-
-    result += sum(a for a in range(start, end + 1) if is_invalid_p2(str(a)))
+result = sum(
+    sum(a for a in range(start, end + 1) if is_invalid_p2(str(a)))
+    for start, end in intervals
+)
 print("Part 2:", result)
